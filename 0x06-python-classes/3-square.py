42b7546620b5dbe054d10d678d25e5d0fda3d050
AlinafeICK <alinafeick@gmail.com>
2024-04-16 00:19:07 +0200

    Upgraded our class to handle a public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" A module of class Square based on 1-square.py."""
+
+
+class Square:
+    """A Square Class that defines a square
+    with a private instance attribute: size
+    """
+    def __init__(self, size=0):
+        """ Initializes a new Square.
+        Args:
+            size (int): Size of new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
