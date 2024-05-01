a7580998647b50bfb2c5412a43ee08aea25f8b76
AlinafeICK <alinafeick@gmail.com>
2024-05-01 13:09:31 +0200

    A function that prints a square with '#' Character

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,9 +16,9 @@ def say_my_name(first_name, last_name=""):
     Raise:
     TypeError: If either first_name or last_name is not a string
     """
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, (str,)):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if not isinstance(last_name, (str,)):
         raise TypeError("last_name must be a string")
 
     if last_name:
