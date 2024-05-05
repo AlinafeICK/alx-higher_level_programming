380fa5105baf045c1781bc39971132f98a5c98a0
AlinafeICK <alinafeick@gmail.com>
2024-05-05 17:19:22 +0200

    Everything is an Object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-"""A function that
-    copies a list
-    and returns a new list
-    with a copy of the original list
-    without modifying the original list.
-"""
-
-
 def copy_list(my_list):
    return my_list.copy()
