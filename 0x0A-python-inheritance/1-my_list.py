c6f9bf4e557e23fb93c23b43e78414b9011cf6db
AlinafeICK <alinafeick@gmail.com>
2024-05-06 08:17:24 +0200

    inheritance 101

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" a Class Myclass that inherits from list """
+
+
+class MyList(list):
+    """ a Class Myclass that inherits from list """
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort) """
+        print(sorted(self))
