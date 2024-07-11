00508b65ad6f22349916cf177087b780f3db9d45
AlinafeICK <alinafeick@gmail.com>
2024-07-12 00:32:01 +0200

    inheritance scripts

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Defines a class BaseGeometry"""
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
