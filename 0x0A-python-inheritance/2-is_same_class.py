651a00f9b232e2e64b3c9fc0eb62343a37df0dd3
AlinafeICK <alinafeick@gmail.com>
2024-07-11 23:25:11 +0200

    Updated 2-is_same_class to pass requirement check

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" A function that returns True if the object
+""" A function that True if the object
 is exactly an instance of the specified class"""
 
 
 def is_same_class(obj, a_class):
-    """Function that returns True if the object
+    """Function that checks if the object
     is exactly an instance of the specified class"""
     return (type(obj) == a_class)
