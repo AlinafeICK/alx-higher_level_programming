7e7ad5ff1567273cff9a896fd6204946598ef320
AlinafeICK <alinafeick@gmail.com>
2024-07-22 03:39:17 +0200

    Listing specific records in order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- 10-top_score.sql
+-- Lists all records of the table second_table in order
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- 8-count_89.sql
+-- Count the number of records with id = 89 from table
+SELECT COUNT(*) AS record_count
+FROM first_table
+WHERE id = 89;
