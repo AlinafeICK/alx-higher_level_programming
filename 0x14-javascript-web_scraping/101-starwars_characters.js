b595f83276841e17b5e7f3d687cf4f9c5fe785d2
AlinafeICK <alinafeick@gmail.com>
2024-07-31 04:38:45 +0200

    Updated 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // a script that prints all characters of a Star Wars movie
+
 const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
@@ -7,19 +8,20 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+    return;
   }
+
+  const content = JSON.parse(body);
+  const characters = content.characters;
+
+  characters.forEach(character => {
+    request.get(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const names = JSON.parse(body);
+        console.log(names.name);
+      }
+    });
+  });
 });
