c8a161506783e330827df369cace38973de9f373
AlinafeICK <alinafeick@gmail.com>
2024-08-03 22:54:50 +0200

    Removes braces from the return value

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ def append_write(filename="", text=""):
     """A function that appends a string at the end of a file
     and returns the number of characters added"""
     with open(filename, "a", encoding="utf-8") as f:
-        return (f.write(text))
+        return f.write(text)
