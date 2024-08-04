b5c2199a6524b7a0710db9dd3b0920b0bab291ce
AlinafeICK <alinafeick@gmail.com>
2024-08-04 13:28:28 +0200

    Handling Json files

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """A function that returns the JSON representation of an object (string)"""
 import json
 
+
 def to_json_string(my_obj):
     """A function that returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
